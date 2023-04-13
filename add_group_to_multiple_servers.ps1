baa108f477bf8f6614953f8e79784bcf2ce9f4af
vgiordano33 <130619484+vgiordano33@users.noreply.github.com>
2023-04-13 11:03:13 +1000

    Update add_group_to_multiple_servers.ps1

--- add_group_to_multiple_servers.ps1
+++ add_group_to_multiple_servers.ps1
@@ -1,8 +1,8 @@
 # Define the servers to add the group to
-$servers = @("SYVWTDBSTL25","SYVWTDB115X64","syvwpdbapgarr01","SYDGARTS02","SYVWTDBGARRAD22","sydgar121d","SYVWDBNTRSRT01","SYVWTDB117X64","SYVWTVDIMPL","SYVWTDBGARRAD26","SYVWTDBGARRAD21","syvwtdbgarrad27","sydgarradin17-clone","syvotdb117mrc_2","syvwtdbgarrad23","syvwtbusmtest02","syvrtdb117-mbl_old","syvctdb117_mrc_restored","SYVWTDBNTRSPR","syvotdb117x64","syvrtdb117-mbl","syswddbgar121","syvotdb117mrt","SYVWTDBOE117X64","sydgarradin16","SYDGARRADIN15","syvwtdbgarrad24","syvwdapgarrad01","SYVWDDBGARARD01","sydgarradin13","SYVWPAPGARRAD01","syvcdwwgarsvn02","syvodapgit01","sydgarfs01","sydhotdesk11","syswtapgarrad01")
+$servers = @("server1","server2")
 
 # Define the group to add
-$group = "security_garradin_access"
+$group = "groupname"
 
 # Loop through each server and add the group
 foreach ($server in $servers) {
